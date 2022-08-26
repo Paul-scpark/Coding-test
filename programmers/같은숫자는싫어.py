4a58a9c48683ec57bfa8f36497f68c0e2d66a0ef
Park Seongchan <75621410+Paul-scpark@users.noreply.github.com>
2022-08-27 02:02:17 +0900

    Update 같은숫자는싫어.py

--- programmers/같은숫자는싫어.py
+++ programmers/같은숫자는싫어.py
@@ -5,6 +5,7 @@
 ### https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3
 
 def solution(arr):
+    
     idx = 0 ; arr_lst = []
     
     # 현재 인덱스와 다음 인덱스가 동일하지 않을 때까지 idx 증가시키고, arr_lst에 값을 append
@@ -15,4 +16,5 @@ def solution(arr):
     
     # 가장 마지막 항목은 누락되기 때문에 추가로 붙여주기
     arr_lst.append(arr[-1])
+    
     return arr_lst
