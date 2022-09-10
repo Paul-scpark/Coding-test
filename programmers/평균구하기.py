7522f6d8d18cb80fe4838971cb021059a2aeb900
Park Seongchan <75621410+Paul-scpark@users.noreply.github.com>
2022-09-10 22:48:35 +0900

    Add 220910 평균 구하기 코테

---
+++ programmers/평균구하기.py
@@ -0,0 +1,10 @@
+# 프로그래머스 코딩테스트 연습문제 - python
+
+### 체감 난이도: 1
+### numpy 활용
+### https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+import numpy as np
+
+def solution(arr):
+    return np.array(arr).mean()
