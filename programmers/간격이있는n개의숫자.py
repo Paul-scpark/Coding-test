6b059a7af07c9bb3cb139168b1d1ffc8e6688048
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-09-13 19:45:37 +0900

    Add 220913 코테 문제 풀이이

---
+++ programmers/간격이있는n개의숫자.py
@@ -0,0 +1,9 @@
+# 프로그래머스 코딩테스트 연습문제 - python
+
+### 체감 난이도: 2
+### for loop 효율적으로 작성하는 방법
+### https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+def solution(x, n):
+
+    return [x*i for i in range(1, n+1)]
---
+++ programmers/두정수사이의합.py
@@ -0,0 +1,11 @@
+# 프로그래머스 코딩테스트 연습문제 - python
+
+### 체감 난이도: 2
+### 반복문 없이 합 구하는 방법, range 함수의 활용
+### https://school.programmers.co.kr/learn/courses/30/lessons/12912
+
+def solution(a, b):
+    
+    if a > b: a, b = b, a
+    
+    return sum(range(a, b + 1))
