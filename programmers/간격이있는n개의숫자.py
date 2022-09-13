ff31c80404133e57e7d0fdca359de43429fbb89f
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-09-13 19:47:32 +0900

    Add 220913 코테 문제 풀이

--- programmers/간격이있는n개의숫자.py
+++ programmers/간격이있는n개의숫자.py
@@ -1,7 +1,7 @@
 # 프로그래머스 코딩테스트 연습문제 - python
 
 ### 체감 난이도: 2
-### for loop 효율적으로 작성하는 방법
+### for loop 효율적으로 작성하는 방법 (간격 추가)
 ### https://school.programmers.co.kr/learn/courses/30/lessons/12954
 
 def solution(x, n):
--- programmers/두정수사이의합.py
+++ programmers/두정수사이의합.py
@@ -1,7 +1,7 @@
 # 프로그래머스 코딩테스트 연습문제 - python
 
 ### 체감 난이도: 2
-### 반복문 없이 합 구하는 방법, range 함수의 활용
+### 반복문 없이 총합 구하는 방법, range 함수의 활용
 ### https://school.programmers.co.kr/learn/courses/30/lessons/12912
 
 def solution(a, b):
