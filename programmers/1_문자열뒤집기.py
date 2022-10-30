e812066fbae61769d38da96f8fd5ba016dc18ce1
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-10-30 22:04:44 +0900

    Add 221030 코테 문제 풀이

---
+++ programmers/1_문자열뒤집기.py
@@ -0,0 +1,8 @@
+# 프로그래머스 코딩테스트 연습문제 - python
+
+### 체감 난이도: 1
+### 문자열 reverse 함수 [::-1]
+### https://school.programmers.co.kr/learn/courses/30/lessons/120822
+
+def solution(my_string):
+    return my_string[::-1]
