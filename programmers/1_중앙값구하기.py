d6a83f969df2306806e82d0ad0060db61951fdea
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-11-03 23:57:26 +0900

    Add 221103 코테 문제 풀이

---
+++ programmers/1_중앙값구하기.py
@@ -0,0 +1,8 @@
+# 프로그래머스 코딩테스트 연습문제 - python
+
+### 체감 난이도: 1
+### list 활용
+### https://school.programmers.co.kr/learn/courses/30/lessons/120811?language=python3
+
+def solution(array):    
+    return sorted(array)[int(len(array) / 2)]
