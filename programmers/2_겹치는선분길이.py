43dc8db40e600b0ee85138ba1fed0cffa3001302
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-11-19 23:54:31 +0900

    Add 221119 코테 문제 풀이

--- programmers/2_겹치는선분길이.py
+++ programmers/2_겹치는선분길이.py
@@ -7,13 +7,19 @@
 from itertools import combinations
 
 def solution(lines):    
+    # 각 선분 별로 range 함수로 길이를 담고 있는 리스트 생성
     lst = [range(i[0], i[1] + 1) for i in lines]
-    
+
     combination_lst = []
+    # 두 개 이상의 선분이 겹쳐야 하므로, combinations 함수 사용
+    # range 함수로 되어 있는 두 선분의 교집합을 추출해주기
+    # 길이를 담고 있어야 하기 때문에 교집합인 temp의 길이가 1보다 큰 값만 추출
     for i in combinations(lst, 2):
         temp = list(set(i[0]) & set(i[1]))
         if len(temp) > 1:
             combination_lst += temp
-    
+
+    # 최종적으로 추출된 combination_lst에 중복 제거 및 sorting
+    # 길이를 계산해야 하므로, 리스트의 길이에 1을 빼서 return
     answer = len(sorted(list(set(combination_lst)))) - 1 
     return answer if answer > 0 else 0
